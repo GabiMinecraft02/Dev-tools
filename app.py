d3917c9a812dd52f6671a267d317a2fee9e92787
GabrielMinecraft02 <gabrielmersesse09@gmail.com>
2025-12-27 21:42:03 +0100

    Premier commit

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, send_from_directory
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/robots.txt")
+def robots():
+    return send_from_directory('.', 'robots.txt')
+
+@app.route("/sitemap.xml")
+def sitemap():
+    return send_from_directory('.', 'sitemap.xml')
+
+@app.route("/googlea128813747473c36.html")
+def google_verification():
+    return send_from_directory('.', 'googlea128813747473c36.html')
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
---
+++ run.bat
@@ -0,0 +1,8 @@
+@echo off
+echo -------------------
+echo run.bat...        -
+echo -------------------
+echo démarage de app.py-
+echo --------------------
+python app.py
+pause
